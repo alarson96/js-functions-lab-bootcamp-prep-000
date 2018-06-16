1848004de0f1d07dfec988813b23ee1945798242
alarson96@outlook.com <alarson96@outlook.com>
2018-06-16 01:29:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ function happyHolidaysTo()
 
 function happyHolidayTo(holiday, name)
 {
-  return "Happy ${holiday}, ${name};"
+  return "Happy ${holiday}, ${name}!"
 }
 
 function holidayCountdown(holiday, days)
 {
-  return "It's ${days} days until${holiday}"
+  return "It's ${days} days until${holiday}!"
 }
